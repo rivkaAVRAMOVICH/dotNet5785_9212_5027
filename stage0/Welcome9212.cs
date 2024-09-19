52132490ecaae2449d52dde51dd69ea91d8fa013
rivkaAVRAMOVICH <mavramovich7@gmail.com>
2024-09-19 19:42:58 +0300

    Initial Code + Welcome functions

---
+++ stage0/Welcome9212.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Welcome9212();
+        Welcome5027();
+        Console.ReadKey();
+    }
+    static partial void Welcome5027();
+    private static void Welcome9212()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", userName);
+    }
+}
