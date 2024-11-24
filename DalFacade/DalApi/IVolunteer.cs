3ba674c17bce2f6232e174eb5d87c08ee1c57c0b
Shani-Schiff <s0534107188@gmail.com>
2024-11-24 14:24:58 +0200

    1

--- DalFacade/DalApi/IVolunteer.cs
+++ DalFacade/DalApi/IVolunteer.cs
@@ -1,7 +1,7 @@
 ﻿namespace DalApi;
 using DO;
 
-internal interface IVolunteer
+public interface IVolunteer
 {
     void Create(Volunteer item); //Creates new entity object in DAL
     Volunteer? Read(int id); //Reads entity object by its ID
