4fba8d8c55e0600c7a51e2a2f94fa9b8a1ec65e0
Shani-Schiff <s0534107188@gmail.com>
2025-05-20 00:02:43 +0300

    .

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Threading;
 
 namespace PL
 {
@@ -16,9 +18,78 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            //Start updating time
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) => CurrentTime = DateTime.Now;
+            timer.Start();
         }
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.MINUTE);
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.HOUR);
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.DAY);
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.MONTH);
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceClock(BO.TimeUnit.YEAR);
+            CurrentTime = s_bl.Admin.GetClock();
+        }
+
+        public TimeSpan MaxYearRange
+        {
+            get { return (TimeSpan)GetValue(MaxYearRangeProperty); }
+            set { SetValue(MaxYearRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxYearRangeProperty =
+            DependencyProperty.Register("MaxYearRange", typeof(int), typeof(MainWindow));
+
+        private void btnUpdateMaxRange_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                s_bl.Admin.SetRiskTimeRange(MaxYearRange);
+                MessageBox.Show("The value updated", "OK", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error was occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
     }
 }
